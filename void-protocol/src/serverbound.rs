ca52c41b94043b9c464935556a4be4896ef6406c
Adam Brutsaert <brutsaertadam@yahoo.fr>
2025-03-14 12:28:56 +0100

    feat(void-protocol): added StatusPacket enum and the StatusRequest and PingRequest variants

--- void-protocol/src/serverbound.rs
+++ void-protocol/src/serverbound.rs
@@ -68,3 +68,75 @@ impl Packet for HandshakePacket {
         }
     }
 }
+
+#[derive(Debug)]
+pub struct StatusRequest {}
+
+impl Packet for StatusRequest {
+    fn encode<E: PacketEncode>(&self, _encoder: &mut E) -> std::io::Result<()> {
+        Ok(())
+    }
+
+    fn decode<D: PacketDecode>(_decoder: &mut D) -> std::io::Result<Self> {
+        Ok(Self {})
+    }
+}
+
+impl PacketId for StatusRequest {
+    const ID: i32 = 0x00;
+}
+
+#[derive(Debug)]
+pub struct PingRequest {
+    pub timestamp: i64,
+}
+
+impl Packet for PingRequest {
+    fn encode<E: PacketEncode>(&self, encoder: &mut E) -> std::io::Result<()> {
+        encoder.encode_i64(self.timestamp)
+    }
+
+    fn decode<D: PacketDecode>(decoder: &mut D) -> std::io::Result<Self> {
+        let timestamp = decoder.decode_i64()?;
+
+        Ok(Self { timestamp })
+    }
+}
+
+impl PacketId for PingRequest {
+    const ID: i32 = 0x01;
+}
+
+#[derive(Debug)]
+pub enum StatusPacket {
+    StatusRequest(StatusRequest),
+    PingRequest(PingRequest),
+}
+
+impl Packet for StatusPacket {
+    fn encode<E: PacketEncode>(&self, encoder: &mut E) -> std::io::Result<()> {
+        match self {
+            StatusPacket::StatusRequest(packet) => {
+                encoder.encode_vari32(StatusRequest::ID)?;
+                packet.encode(encoder)
+            }
+            StatusPacket::PingRequest(packet) => {
+                encoder.encode_vari32(PingRequest::ID)?;
+                packet.encode(encoder)
+            }
+        }
+    }
+
+    fn decode<D: PacketDecode>(decoder: &mut D) -> std::io::Result<Self> {
+        let id = decoder.decode_vari32()?;
+
+        match id {
+            StatusRequest::ID => Ok(StatusPacket::StatusRequest(StatusRequest::decode(decoder)?)),
+            PingRequest::ID => Ok(StatusPacket::PingRequest(PingRequest::decode(decoder)?)),
+            _ => Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                format!("Unknown packet ID: {}", id),
+            )),
+        }
+    }
+}
