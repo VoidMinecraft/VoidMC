f6ce271b12ae67e7a1a5fc3c5a924100b4ceefa7
Adam Brutsaert <brutsaertadam@yahoo.fr>
2026-01-30 15:34:41 +0100

    refactor: splitted handle functions

--- void/src/network.rs
+++ void/src/network.rs
@@ -59,113 +59,6 @@ pub struct State(pub void_protocol::State);
 #[derive(Component)]
 pub struct ProtocolVersion(pub i32);
 
-pub fn send_packet(world: &mut World, packet: OutgoingPacket) -> std::io::Result<()> {
-    world.resource_scope(|_world, channels: Mut<NetworkChannels>| {
-        channels
-            .outgoing
-            .send(packet)
-            .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))
-    })
-}
-
-pub fn handle_handshake_packet(
-    world: &mut World,
-    client_entity: Entity,
-    packet: serverbound::HandshakePacket,
-) -> std::io::Result<()> {
-    match packet {
-        serverbound::HandshakePacket::Handshake(handshake) => {
-            // Store the protocol version in a component
-            world
-                .entity_mut(client_entity)
-                .insert(ProtocolVersion(handshake.protocol_version));
-
-            // Overwrite the state component to the next state
-            world
-                .entity_mut(client_entity)
-                .insert(State(handshake.next_state));
-        }
-    }
-
-    Ok(())
-}
-
-pub fn handle_status_packet(
-    world: &mut World,
-    client_id: u32,
-    client_entity: Entity,
-    packet: serverbound::StatusPacket,
-) -> std::io::Result<()> {
-    match packet {
-        serverbound::StatusPacket::StatusRequest(_) => send_packet(
-            world,
-            OutgoingPacket {
-                client_id,
-                packet: clientbound::ClientboundPacket::Status(
-                    clientbound::StatusPacket::StatusResponse(clientbound::StatusResponse {
-                        status: clientbound::Status {
-                            version: clientbound::Version {
-                                name: "Void Server".to_string(),
-                                protocol: world
-                                    .get::<ProtocolVersion>(client_entity)
-                                    .expect("Client must have a ProtocolVersion component at this point")
-                                    .0,
-                            },
-                            players: clientbound::Players {
-                                max: 100,
-                                online: 0,
-                                sample: vec![],
-                            },
-                            description: clientbound::Description {
-                                text: "Welcome to Void Server!".to_string(),
-                            },
-                            favicon: "".to_string(),
-                            enforces_secure_chat: false,
-                        },
-                    }),
-                ),
-            },
-        ),
-        serverbound::StatusPacket::PingRequest(ping) => send_packet(
-            world,
-            OutgoingPacket {
-                client_id,
-                packet: clientbound::ClientboundPacket::Status(
-                    clientbound::StatusPacket::PingResponse(clientbound::PingResponse {
-                        timestamp: ping.timestamp,
-                    }),
-                ),
-            },
-        ),
-    }
-}
-
-pub fn handle_packet(
-    world: &mut World,
-    client_id: u32,
-    client_entity: Entity,
-    packet: Packet,
-) -> std::io::Result<()> {
-    let state = world
-        .get::<State>(client_entity)
-        .expect("Client must have a State component");
-
-    match state.0 {
-        void_protocol::State::Handshake => handle_handshake_packet(
-            world,
-            client_entity,
-            packet.decode::<serverbound::HandshakePacket>()?,
-        ),
-        void_protocol::State::Status => handle_status_packet(
-            world,
-            client_id,
-            client_entity,
-            packet.decode::<serverbound::StatusPacket>()?,
-        ),
-        _ => unimplemented!(),
-    }
-}
-
 pub fn ingest_network_packets(world: &mut World) {
     loop {
         let Some(incoming_packet) =
@@ -202,4 +95,142 @@ pub fn ingest_network_packets(world: &mut World) {
             );
         }
     }
+}
+
+pub fn send_packet(world: &mut World, packet: OutgoingPacket) -> std::io::Result<()> {
+    world.resource_scope(|_world, channels: Mut<NetworkChannels>| {
+        channels
+            .outgoing
+            .send(packet)
+            .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))
+    })
+}
+
+pub fn handle_packet(
+    world: &mut World,
+    client_id: u32,
+    client_entity: Entity,
+    packet: Packet,
+) -> std::io::Result<()> {
+    let state = world
+        .get::<State>(client_entity)
+        .expect("Client must have a State component");
+
+    match state.0 {
+        void_protocol::State::Handshake => handle_handshake_packet(
+            world,
+            client_entity,
+            packet.decode::<serverbound::HandshakePacket>()?,
+        ),
+        void_protocol::State::Status => handle_status_packet(
+            world,
+            client_id,
+            client_entity,
+            packet.decode::<serverbound::StatusPacket>()?,
+        ),
+        _ => unimplemented!(),
+    }
+}
+
+pub fn handle_handshake_packet(
+    world: &mut World,
+    client_entity: Entity,
+    packet: serverbound::HandshakePacket,
+) -> std::io::Result<()> {
+    match packet {
+        serverbound::HandshakePacket::Handshake(handshake) => {
+            handle_handshake_packet_handshake(world, client_entity, handshake)
+        }
+    }
+}
+
+pub fn handle_handshake_packet_handshake(
+    world: &mut World,
+    client_entity: Entity,
+    packet: serverbound::Handshake,
+) -> std::io::Result<()> {
+    // Store the protocol version in a component
+    world
+        .entity_mut(client_entity)
+        .insert(ProtocolVersion(packet.protocol_version));
+
+    // Overwrite the state component to the next state
+    world
+        .entity_mut(client_entity)
+        .insert(State(packet.next_state));
+
+    Ok(())
+}
+
+pub fn handle_status_packet(
+    world: &mut World,
+    client_id: u32,
+    client_entity: Entity,
+    packet: serverbound::StatusPacket,
+) -> std::io::Result<()> {
+    match packet {
+        serverbound::StatusPacket::StatusRequest(packet) => {
+            handle_status_packet_status(world, client_id, client_entity, packet)
+        }
+        serverbound::StatusPacket::PingRequest(packet) => {
+            handle_status_packet_ping(world, client_id, packet)
+        }
+    }
+}
+
+pub fn handle_status_packet_status(
+    world: &mut World,
+    client_id: u32,
+    client_entity: Entity,
+    _packet: serverbound::StatusRequest,
+) -> std::io::Result<()> {
+    send_packet(
+        world,
+        OutgoingPacket {
+            client_id,
+            packet: clientbound::ClientboundPacket::Status(
+                clientbound::StatusPacket::StatusResponse(clientbound::StatusResponse {
+                    status: clientbound::Status {
+                        version: clientbound::Version {
+                            name: "Void Server".to_string(),
+                            protocol: world
+                                .get::<ProtocolVersion>(client_entity)
+                                .expect(
+                                    "Client must have a ProtocolVersion component at this point",
+                                )
+                                .0,
+                        },
+                        players: clientbound::Players {
+                            max: 100,
+                            online: 0,
+                            sample: vec![],
+                        },
+                        description: clientbound::Description {
+                            text: "Welcome to Void Server!".to_string(),
+                        },
+                        favicon: "".to_string(),
+                        enforces_secure_chat: false,
+                    },
+                }),
+            ),
+        },
+    )
+}
+
+pub fn handle_status_packet_ping(
+    world: &mut World,
+    client_id: u32,
+    packet: serverbound::PingRequest,
+) -> std::io::Result<()> {
+    send_packet(
+        world,
+        OutgoingPacket {
+            client_id,
+            packet: clientbound::ClientboundPacket::Status(
+                clientbound::StatusPacket::PingResponse(clientbound::PingResponse {
+                    timestamp: packet.timestamp,
+                }),
+            ),
+        },
+    )
 }
